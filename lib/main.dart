acaf60e6c04e19c03b878402ea2bf1e349407ebd
wonappy <wonappy@naver.com>
2025-04-21 15:55:48 +0900

    update main.dart
    필요없는 import문 정리

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:sense_voka/screens/create_mywordbook_screen.dart';
-import 'package:sense_voka/screens/main_screen.dart';
-import 'package:sense_voka/screens/main_wordbook_screen.dart';
-import 'package:sense_voka/screens/mywordbook_screen.dart';
 import 'package:sense_voka/screens/sign_in_screen.dart';
-import 'package:sense_voka/screens/sign_up_screen.dart';
-import 'package:sense_voka/screens/word_study_screen.dart';
 
 void main() {
   runApp(App());
